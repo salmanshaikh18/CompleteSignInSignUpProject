924142b4588866cf486b665a62b47853aa145dcc
Salman Shaikh <salmanshaikh18786@gmail.com>
2024-07-15 21:37:35 +0530

    shadcn ui setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./App.css"
+import { Button } from './components/ui/button'
+import { ThemeProvider } from './components/ui/theme-provider'
 
 const App = () => {
   return (
-    <div>App</div>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <Button variant={"secondary"}>Click</Button>
+    </ThemeProvider>
   )
 }
 
